dab41939a53298ef1918ab8243d5eba47883e1ab
lrmuyi <1342940006@qq.com>
2018-12-12 16:40:00 +0800

    删除项目

---
+++ src/api/login.js
@@ -0,0 +1,8 @@
+import http from '@/utils/fetch'
+
+export const login = () => {
+  return http({
+    url: '',
+    method: 'get'
+  })
+}
---
+++ src/views/System/ContentCom.vue
@@ -0,0 +1,21 @@
+<template>
+  <Content :style="{padding: '0 16px 16px'}">
+    <Breadcrumb :style="{margin: '16px 0'}">
+      <BreadcrumbItem>Home</BreadcrumbItem>
+      <BreadcrumbItem>Components</BreadcrumbItem>
+      <BreadcrumbItem>Layout</BreadcrumbItem>
+    </Breadcrumb>
+    <Card>
+      <div style="height: 600px">Content</div>
+    </Card>
+  </Content>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
